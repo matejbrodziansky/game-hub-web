7e68cfcb626771b106d58b4145277d72c913a12e
Matej Brodziansky <matejbrodziansky@gmail.com>
2025-03-29 11:06:16 +0100

    CheckShapesCollision Method

--- src/games/tetris/logic/useCollision.tsx
+++ src/games/tetris/logic/useCollision.tsx
@@ -10,19 +10,22 @@ const useCollision = () => {
 
     const checkCollision = (position: { x: number, y: number }, shapeOffsets: shapeOffsetsType) => {
         const lowestPartOfShape = getLowestPartOfShapeOffset(shapeOffsets)
-        if (checkBottomCollision(position.y + lowestPartOfShape)) {
-            return true
-        }
 
+        if (checkBottomCollision(position.y + lowestPartOfShape)) return true
+
+        if (checkShapesCollision(shapeOffsets, position)) return true
+
+        return false
+    }
+
+    const checkShapesCollision = (shapesOffset: shapeOffsetsType, position: { x: number, y: number }) => {
         const FALL_OFFSET = 1;
-        for (const [dx, dy] of shapeOffsets) {
+        for (const [dx, dy] of shapesOffset) {
             const positionKey = `${position.x + dx},${position.y + dy + FALL_OFFSET}`;
             if (filledCells.has(positionKey)) {
                 return true;
             }
         }
-
-        return false
     }
 
     const checkBottomCollision = (position: number) => {
